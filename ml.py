ca7efe0bc0c58780eb86ec2126e4cdf8e1138d9b
Levi <90584200+LeviBcodes@users.noreply.github.com>
2021-09-25 16:27:26 -0700

    created a decision tree model, building the decision tree using its fit function

--- ml.py
+++ ml.py
@@ -1,3 +1,7 @@
 from sklearn.datasets import load_iris
 iris = load_iris()
 print(list(iris.target_names))
+
+from sklearn import tree
+classifier = tree.DesicionTreeClassifier()
+classifier = classifier.fit(iris.data, iris.target)
